e755195577a2d6e27fd98d211ebb74692ce90330
ednaleiden <ednadupont@hotmail.com>
2024-11-04 13:56:42 -0500

    config components an task list

--- src/app/components/tarea-list/tarea-list.component.ts
+++ src/app/components/tarea-list/tarea-list.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../../models/tarea';
+import { TareaService } from '../../services/tarea.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tarea-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tarea-list.component.html',
   styleUrl: './tarea-list.component.css'
 })
 export class TareaListComponent {
+
+  tareas?:Tarea[];
+
+  constructor(private tareaServices:TareaService){
+
+  }
+
+
+  ngOnInit():void{
+    this.cargarTareas();
+  }
+
+  cargarTareas():void{
+    this.tareaServices.getAllTareas().subscribe(tareas =>{
+      this.tareas = tareas;
+    } )
+  }
+
+  eliminarTarea(id?:number):void{
+    this.tareaServices.deleteTarea(id!).subscribe(() =>{
+      this.cargarTareas();
+    })
+  }
+
 
 }
