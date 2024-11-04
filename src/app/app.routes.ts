80d54fc8eae64084a7539ca7b7a713e02d96b678
ednaleiden <ednadupont@hotmail.com>
2024-11-04 11:57:07 -0500

    include components and routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TareaListComponent } from './components/tarea-list/tarea-list.component';
+import { TareaFormComponent } from './components/tarea-form/tarea-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'/task', pathMatch:'full'},
+  {path:'task', component:TareaListComponent},
+  {path:'task/new', component:TareaFormComponent},
+  {path:'task/:id/edit', component:TareaFormComponent},
+];
